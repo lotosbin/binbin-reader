02c9c3b150fc73fe2c1949b565e8ec85d7755094
liubinbin <lotosbin@gmail.com>
2015-05-06 23:51:28 +0800

    refactor

--- server/routes/feeds.js
+++ server/routes/feeds.js
@@ -1,36 +1,14 @@
 var express = require('express');
 var router = express.Router();
-
+var service = require('../services/feedService.js');
 /* GET users listing. */
 router.get('/import', function(req, res, next) {
-  var OpmlParser  = require('opmlparser');
-  var opmlparser = new OpmlParser();
-  var request = require('request');
-  var req = request('http://localhost:3333/opml/opml.xml');
-  req.on('error',function(error){
-    console.log('error:'+JSON.stringify(error));
-  });
-  opmlparser.on('error',function(error){
-    console.log('error:'+JSON.stringify(error));
-  })
-  opmlparser.on('readable',function(){
-    var stream = this;
-    var meta = this.meta;
-    console.log('meta:'+JSON.stringify(meta));
-    var outline;
-    while(outline = stream.read()){
+  service.getFeeds(function(data) {
+    for (var i = 0; i < data.length; i++) {
+      var outline = data[i];
       console.log(outline);
     }
   });
-  req.on('response',function(res){
-    console.log('response:'+res.statusCode);
-    var stream = this;
-    if(res.statusCode != 200){
-      return this.emit('error',new Error('Bad status code'));
-    }
-    stream.pipe(opmlparser);
-  });
-
   res.send('respond with a resource');
 });
 
---
+++ server/services/feedService.js
@@ -0,0 +1,36 @@
+var s = {};
+s.getFeeds = function(callback) {
+  var OpmlParser = require('opmlparser');
+  var opmlparser = new OpmlParser();
+  var request = require('request');
+  var req = request('http://localhost:3333/opml/opml.xml');
+  req.on('error', function(error) {
+    console.log('error:' + JSON.stringify(error));
+  });
+  opmlparser.on('error', function(error) {
+    console.log('error:' + JSON.stringify(error));
+  });
+  opmlparser.on('readable', function() {
+    var stream = this;
+    var meta = this.meta;
+    console.log('meta:' + JSON.stringify(meta));
+    var outlines = [];
+    var outline;
+    while (outline = stream.read()) {
+      outlines.push(outline);
+      console.log(outline);
+    }
+    if (callback) {
+      callback(outlines);
+    }
+  });
+  req.on('response', function(res) {
+    console.log('response:' + res.statusCode);
+    var stream = this;
+    if (res.statusCode != 200) {
+      return this.emit('error', new Error('Bad status code'));
+    }
+    stream.pipe(opmlparser);
+  });
+}
+module.exports = s;
