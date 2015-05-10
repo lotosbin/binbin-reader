5762ad80c39417b5581a4d3b68e2530db7535309
liubinbin <lotosbin@gmail.com>
2015-05-10 17:57:05 +0800

    feed index

--- server/repositories/feedRepository.js
+++ server/repositories/feedRepository.js
@@ -1,35 +1,25 @@
 var r = {};
-r.connect = function() {
+r.connect = function(callback) {
   var MongoClient = require('mongodb').MongoClient,
     assert = require('assert');
 
   // Connection URL
-  var url = 'mongodb://localhost:27017/myproject';
+  var url = 'mongodb://localhost:27017/reader';
   // Use connect method to connect to the Server
   MongoClient.connect(url, function(err, db) {
-    assert.equal(null, err);
-    console.log("Connected correctly to server");
-
-    db.close();
+    if (callback) callback(err, db);
+    else
+      db.close();
   });
 };
-r.add = function() {
+r.add = function(outline, callback) {
   var insertDocuments = function(db, callback) {
     // Get the documents collection
     var collection = db.collection('reader');
     // Insert some documents
-    collection.insert([{
-      a: 1
-    }, {
-      a: 2
-    }, {
-      a: 3
-    }], function(err, result) {
-      assert.equal(err, null);
-      assert.equal(3, result.result.n);
-      assert.equal(3, result.ops.length);
-      console.log("Inserted 3 documents into the document collection");
-      callback(result);
+    collection.insert([outline], function(err, result) {
+      if (callback)
+        callback(result);
     });
   }
 };
--- server/routes/feeds.js
+++ server/routes/feeds.js
@@ -1,15 +1,20 @@
 var express = require('express');
 var router = express.Router();
-var service = require('../services/feedService.js');
+var service = require('../services/feedService');
 /* GET users listing. */
 router.get('/import', function(req, res, next) {
   service.getFeeds(function(data) {
     for (var i = 0; i < data.length; i++) {
       var outline = data[i];
       console.log(outline);
+      var repo = require('../repositories/feedRepository');
+      repo.add(outline);
     }
   });
   res.send('respond with a resource');
 });
-
+router.get('/index', function(req, res, next) {
+  var outlines = [];
+  res.send(outlines);
+})
 module.exports = router;
