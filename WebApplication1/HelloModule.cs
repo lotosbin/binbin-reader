f30bad95bffab9d4d05c59284c1ebbf1d3148bf6
lotosbin <lotosbin@gmail.com>
2015-05-10 22:40:13 +0800

    init

---
+++ WebApplication1/HelloModule.cs
@@ -0,0 +1,13 @@
+﻿using Nancy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1 {
+    public class HelloModule : NancyModule {
+        public HelloModule() {
+            Get["/"] = parameters => "Hello World";
+        }
+    }
+}
