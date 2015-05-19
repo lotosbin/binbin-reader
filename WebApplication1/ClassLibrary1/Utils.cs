c62c4545feada7812335cf6120f305f938b232d8
lotosbin <lotosbin@gmail.com>
2015-05-19 16:05:44 +0800

    .

---
+++ WebApplication1/ClassLibrary1/Utils.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1 {
+    public class Utils {
+        public static int ChineseLetterCount(string strText) {
+            byte[] byts = Encoding.GetEncoding("gb2312").GetBytes(strText);
+            return byts.Length - strText.Length;
+        }
+    }
+}
---
+++ WebApplication1/ClassLibraryUnitTestProject2/UtilsTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using ClassLibrary1;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ClassLibraryUnitTestProject2 {
+    [TestClass]
+    public class UtilsTest {
+        [TestMethod]
+        public void ChineseLetterCountTest() {
+            var s = "";
+            var count = Utils.ChineseLetterCount(s);
+            Assert.AreEqual(0, count);
+        }
+    }
+}
